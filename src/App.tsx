9b8af74f732e4fb57fa75588fb4c60173f6e5ad5
younghustlingkings-sketch <younghustlingkings@gmail.com>
2025-12-25 12:03:15 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,25 @@
-import { useState } from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import FloatingWhatsApp from './components/FloatingWhatsApp';
-import Home from './pages/Home';
-import About from './pages/About';
-import Services from './pages/Services';
-import Masterclass from './pages/Masterclass';
-import Gallery from './pages/Gallery';
-import Contact from './pages/Contact';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Masterclass from "./pages/Masterclass";
+import Contact from "./pages/Contact";
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('home');
-
-  const renderPage = () => {
-    switch (currentPage) {
-      case 'home':
-        return <Home onNavigate={setCurrentPage} />;
-      case 'about':
-        return <About />;
-      case 'services':
-        return <Services />;
-      case 'masterclass':
-        return <Masterclass />;
-      case 'gallery':
-        return <Gallery />;
-      case 'contact':
-        return <Contact />;
-      default:
-        return <Home onNavigate={setCurrentPage} />;
-    }
-  };
-
   return (
-    <div className="min-h-screen bg-white">
-      <Header currentPage={currentPage} onNavigate={setCurrentPage} />
-      <main>{renderPage()}</main>
-      <Footer />
-      <FloatingWhatsApp />
-    </div>
+    <BrowserRouter>
+      <Header /> {/* LOGO APPEARS ON ALL PAGES */}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/masterclass" element={<Masterclass />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
